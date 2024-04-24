92cd13c7280c76e90085ee9145d6c3bc783584dc
milosz002 <milosz0192@gmail.com>
2024-04-24 20:26:00 +0200

    docker files

--- Finance Tracker/Program.cs
+++ Finance Tracker/Program.cs
@@ -8,12 +8,21 @@ builder.Services.AddHttpClient();
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-// dependency injection
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+// Dependency injection for DbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NBaF5cXmZCekx3WmFZfVpgd19FY1ZTQ2Y/P1ZhSXxXdkZhXH5bc3dXQWBfUE0=");
 
 var app = builder.Build();
+
+// Apply database migrations
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var dbContext = services.GetRequiredService<ApplicationDbContext>();
+    dbContext.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
